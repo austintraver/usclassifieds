b127a810a1c99d927c5d7cc1d509e19acc53eb9d
Cameron Durham <camerondurham@users.noreply.github.com>
2019-11-04 14:26:16 -0800

    Added components for scrollable listings

---
+++ app/src/main/java/com/asparagus/usclassifieds/ScrollableListActivity.java
@@ -0,0 +1,38 @@
+package com.asparagus.usclassifieds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ScrollableListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrollable_list);
+        Intent i = getIntent();
+        populateList();
+    }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    private void populateList()
+    {
+        ArrayList<Listing> listings = Listing.getListings();
+        // Create adapter to convert into views
+        ListingAdapter adapter = new ListingAdapter(this,listings);
+        // Attach adapter to a list of views
+        ListView listView = findViewById(R.id.lvListings);
+        listView.setAdapter(adapter);
+    }
+}
