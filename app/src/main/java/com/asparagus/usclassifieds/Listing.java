2d3a65b52d2400a2f0cee3496e1f65c56c779af7
Julian Zhang <julianzh@usc.edu>
2019-11-04 23:59:21 -0800

    removing category, fixing edit_listing activity

--- app/src/main/java/com/asparagus/usclassifieds/Listing.java
+++ app/src/main/java/com/asparagus/usclassifieds/Listing.java
@@ -7,29 +7,17 @@ public class Listing implements Serializable {
 
     //TODO --> why UUID
     private UUID listingID;
-    private String title, description, category;
+    private String title, description;
     private double price;
     private boolean sold;
     private String image;
     private String ownerID;
 
     /* TODO include image storage */
-    public Listing (String title, String description, String category, String owner, double price) {
+    public Listing (String owner, String title, double price, String description) {
         this.title = title;
         this.description = description;
-        this.category = category;
         this.price = price;
-        this.listingID = UUID.randomUUID();
-        this.ownerID = owner;
-        this.sold = false;
-        this.image = null;
-    }
-
-    public Listing (String title, String description, String category, String owner) {
-        this.title = title;
-        this.description = description;
-        this.category = category;
-        this.price = 0.0;
         this.listingID = UUID.randomUUID();
         this.ownerID = owner;
         this.sold = false;
@@ -43,7 +31,6 @@ public class Listing implements Serializable {
     public UUID getListingID() { return this.listingID; }
     public String getTitle() { return this.title; }
     public String getDescription() { return this.description; }
-    public String getCategory() { return this.category; }
     public double getPrice() { return this.price; }
     public boolean getSold() { return this.sold; }
 }
