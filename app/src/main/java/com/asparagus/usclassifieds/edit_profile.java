3b4f196b8153c9ec573a0d2efbb1b574f376918c
John Tanner <johntannerofficial@users.noreply.github.com>
2019-11-05 03:44:24 -0800

    adding new users works now

--- app/src/main/java/com/asparagus/usclassifieds/edit_profile.java
+++ app/src/main/java/com/asparagus/usclassifieds/edit_profile.java
@@ -2,6 +2,7 @@ package com.asparagus.usclassifieds;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,7 +16,7 @@ import java.util.Map;
 
 public class edit_profile extends AppCompatActivity {
 
-    private static EditText first, last, phone, sNum, sName, city, state, zip;
+    private static EditText first, last, phone, sNum, sName, city, state, zip, desc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +32,7 @@ public class edit_profile extends AppCompatActivity {
         city = findViewById(R.id.city_name);
         state = findViewById(R.id.state_code);
         zip = findViewById(R.id.zip_code);
-
+        desc = findViewById(R.id.description);
 
         if(GlobalHelper.getUser() != null) {
             first.setText(GlobalHelper.getUser().getFirstName());
@@ -42,21 +43,24 @@ public class edit_profile extends AppCompatActivity {
             city.setText(GlobalHelper.getUser().getCity());
             state.setText(GlobalHelper.getUser().getState());
             zip.setText(GlobalHelper.getUser().getZipCode());
+            desc.setText(GlobalHelper.getUser().getDescription());
         }
     }
 
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.update:
-                User updatedUser = new User(GlobalHelper.getEmail(), first.getText().toString(), last.getText().toString(), phone.getText().toString(), GlobalHelper.getUserID(), sNum.getText().toString(), sName.getText().toString(), city.getText().toString(), state.getText().toString(), zip.getText().toString());
-                System.out.println("creating a new/updated user");
-//                Map<String, Object> userUpdates = new HashMap<>();
-//                userUpdates.put("")
-                FirebaseDatabase.getInstance().getReference("users").child(GlobalHelper.getUserID()).setValue(updatedUser);
+                System.out.println("In Onclick for update/create user!");
+                User updatedUser = new User(GlobalHelper.getEmail(), first.getText().toString(), last.getText().toString(), phone.getText().toString(), GlobalHelper.getUserID(), sNum.getText().toString(), sName.getText().toString(), city.getText().toString(), state.getText().toString(), zip.getText().toString(), desc.getText().toString());
+                GlobalHelper.setUser(updatedUser);
+                System.out.println("creating a new/updated user: " + updatedUser);
 
+//                FirebaseDatabase.getInstance().getReference("users").child(GlobalHelper.getUserID()).setValue(updatedUser);
 //                Intent update_user = new Intent();
 //                setResult(105,update_user);
 //                update_user.putExtra("updated_user",updatedUser);
+                Intent newUser = new Intent();
+                setResult(Activity.RESULT_OK, newUser);
                 finish();
                 break;
             case R.id.cancel:
