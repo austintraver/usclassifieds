f6f71a01d401d98f755ce9d07437e324b5a6f97e
John Tanner <johntannerofficial@users.noreply.github.com>
2019-11-05 22:56:47 -0800

    adding other user page functionality

--- app/src/main/java/com/asparagus/usclassifieds/SingleListingActivity.java
+++ app/src/main/java/com/asparagus/usclassifieds/SingleListingActivity.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -28,6 +29,7 @@ public class SingleListingActivity extends Activity {
         System.out.println("Starting Activity: single_listing");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_listing);
+
         Intent intent =  getIntent();
         this.listing = (Listing) intent.getSerializableExtra("listing");
         System.out.println("Listing : " + listing.getTitle() + "\nDescription: " + listing.getDescription());
@@ -39,8 +41,11 @@ public class SingleListingActivity extends Activity {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.other_user_button:
-                Intent intent = new Intent(this, OtherProfileActivity.class);
-                
+                if(listing.getOwnerID() != GlobalHelper.getUserID()) {
+                    Intent intent = new Intent(this, OtherProfileActivity.class);
+                    intent.putExtra("other_user", listing.getOwnerID());
+                    startActivity(intent);
+                }
         }
     }
 
@@ -61,32 +66,36 @@ public class SingleListingActivity extends Activity {
             TextView tvSTitle = findViewById(R.id.detail_title);
             TextView tvSDesc = findViewById(R.id.detail_description);
             TextView tvSPrice = findViewById(R.id.detail_price);
+            Button owner = (Button) findViewById(R.id.other_user_button);
+            TextView desc = findViewById(R.id.detail_description);
             final ImageView ivSListing = findViewById(R.id.listing_image);
             // Load ImageView with photo from firebase per starter code from
             // https://firebase.google.com/docs/storage/android/download-files
-            FirebaseStorage.getInstance().getReference().child(listing.getStorageReference()).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                @Override
-                public void onSuccess(Uri uri) {
-                    try {
-                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-                        ivSListing.setImageBitmap(bitmap);
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception exception) {
-                    // Handle any errors
-                }
-            });
+//            FirebaseStorage.getInstance().getReference().child(listing.getStorageReference()).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+//                @Override
+//                public void onSuccess(Uri uri) {
+//                    try {
+//                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+//                        ivSListing.setImageBitmap(bitmap);
+//                    } catch (FileNotFoundException e) {
+//                        e.printStackTrace();
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }).addOnFailureListener(new OnFailureListener() {
+//                @Override
+//                public void onFailure(@NonNull Exception exception) {
+//                    // Handle any errors
+//                }
+//            });
 //            StorageReference sref = FirebaseStorage.getInstance().getReference().child(listing.getStorageReference());
 //            GlideApp.with(this).load(sref).into(ivSListing);
-//            tvSTitle.setText(listing.getTitle());
-//            tvSDesc.setText(listing.getDescription());
-//            tvSPrice.setText(String.format("$%.2f",listing.getPrice()));
+            tvSTitle.setText(listing.getTitle());
+            tvSDesc.setText(listing.getDescription());
+            tvSPrice.setText(String.format("$%.2f",listing.getPrice()));
+            owner.setText(listing.getOwnerEmail());
+            desc.setText(listing.getDescription());
         }
     }
 }
